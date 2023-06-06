dbe7fd63f786446ecfa093851ba1e58503740bb7
renkayla <116777951+renkayla@users.noreply.github.com>
2023-06-05 20:52:52 -0400

    updated code

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse request body as JSON
+app.use(express.json());
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// GET route to retrieve notes from the JSON file
+app.get('/api/notes', (req, res) => {
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
+
+// POST route to save a new note to the JSON file
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      const notes = JSON.parse(data);
+      newNote.id = Date.now().toString(); // Generate a unique ID for the note
+      notes.push(newNote);
+
+      fs.writeFile(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(notes),
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error' });
+          } else {
+            res.json(newNote);
+          }
+        }
+      );
+    }
+  });
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
